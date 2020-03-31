34a737384b60e1b29b850a4f822ba0f2c1a8307f
calvita <deiby.calva@unl.edu.ec>
2020-03-30 20:15:45 -0500

    introduccion a javaScript

---
+++ Introduccion-JavaScript/js/02_variables.js
@@ -0,0 +1,29 @@
+/*=============================================
+VARIABLES
+=============================================*/
+
+// variabloes de tipo numericas
+var numero = 10;
+console.log("numero", numero);
+
+// variables de tipo texto
+var palabra = "probando intorduccion a javaScript";
+console.log("palabra", palabra);
+
+/// variables Booleanas
+var Booleanas = false;
+console.log("Booleanas", Booleanas);
+
+// variables de tipo Array
+var arreglo = ["1", "2", palabra, Booleanas];
+console.log("arreglo", arreglo);
+console.log("arreglo", arreglo[0]);
+
+// variables de tipo objeto
+var auto = {
+    color: "negro",
+    marca: "chevrolet",
+    año: "2020"
+}
+console.log("auto", auto);
+console.log("auto", auto.marca);
